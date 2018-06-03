8979be41bd6cc4fdb413a1a23aab946ab90aea3b
yinhaomin088 <yinhaomin088@pingan.com.cn>
2018-06-03 21:30:12 +0800

    Added more tests

---
+++ common-test-service/src/main/java/com/baidu/common/test/service/string/LongToString.java
@@ -0,0 +1,11 @@
+package com.baidu.common.test.service.string;
+
+public class LongToString {
+
+	public static void main(String[] args) {
+		Long llll = 1244352362356236L;
+		System.out.println(llll.toString());
+		System.out.println(llll + "");
+	}
+
+}
